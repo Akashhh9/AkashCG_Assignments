04b919eb564e7c6dd34bd3c9e6ae62bd00f4db0f
Akash Choudhary <akash9112002@gmail.com>
2026-04-07 15:11:27 +0530

    uploaded april 6 & 7 assignments with Screenshots

---
+++ Week5/April07-Assignment/WebApplication0704/WebApplication0704/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication0704.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
